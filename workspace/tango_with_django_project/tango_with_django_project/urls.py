402267cacf8adf5a75fb1f37e0cf21487139acc1
cianlaguesma <47908988+cianlaguesma@users.noreply.github.com>
2019-08-04 21:23:07 +0800

    Add files via upload

--- workspace/tango_with_django_project/tango_with_django_project/urls.py
+++ workspace/tango_with_django_project/tango_with_django_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from rango import views
+urlpatterns = [ path('', views.index, name='index'),
+                path('rango/', include('rango.urls')),]
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+# The above maps any URLs starting with rango/ to be handled by the rango app. path('admin/', admin.site.urls),
