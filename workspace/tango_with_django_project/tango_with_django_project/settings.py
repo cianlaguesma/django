8031b53c847d3f27da00bfc0301625892cb19a2e
cianlaguesma <47908988+cianlaguesma@users.noreply.github.com>
2019-08-06 00:31:42 +0800

    added static and dynamic files
    p. 57

--- workspace/tango_with_django_project/tango_with_django_project/settings.py
+++ workspace/tango_with_django_project/tango_with_django_project/settings.py
@@ -15,7 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -28,6 +29,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+#MEDIA PART
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 # Application definition
 
@@ -57,6 +61,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [TEMPLATE_DIR, ],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -64,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -119,4 +125,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
--- workspace/tango_with_django_project/tango_with_django_project/urls.py
+++ workspace/tango_with_django_project/tango_with_django_project/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [ path('', views.index, name='index'),
                 path('rango/', include('rango.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 # The above maps any URLs starting with rango/ to be handled by the rango app. path('admin/', admin.site.urls),
